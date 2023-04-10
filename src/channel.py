4e91cd36524e0d8b8bd65e04a9f486aa6724e594
Slava Kholopov <slava.kholopov@gmail.com>
2023-04-10 12:55:33 +0200

    added more attributes to class  Channel, save file with results

--- src/channel.py
+++ src/channel.py
@@ -13,10 +13,54 @@ def printj(dict_to_print: dict) -> None:
 class Channel:
     """Класс для ютуб-канала"""
 
-    def __init__(self, channel_id: str) -> None:
-        """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
-        self.channel_id = channel_id
 
+    def __init__(self, channel_id: str, title: str, description: str, url: str, subscribers: int, video: int, views: int) -> None:
+        """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
+        self.__channel_id = channel_id
+        self.title = title
+        self.description = description
+        self.url = url
+        self.subscribers = subscribers
+        self.video = video
+        self.views = views
+
+    @classmethod
+    def get_service(cls, channel_id):
+        request = youtube.channels().list(
+            part='snippet',
+            id=channel_id
+        )
+        response = request.execute()
+
+
+        # extract channel title from response data
+        items = response.get('items', [])
+        title = items[0]['snippet']['title']
+        description = items[0]['snippet']['description']
+        url = f'https://www.youtube.com/channel/{channel_id}'
+
+        request = youtube.channels().list(
+            part='statistics',
+            id=channel_id
+        )
+        response = request.execute()
+        items = response.get('items', [])
+        subscribers = items[0]['statistics']['subscriberCount']
+        video = items[0]['statistics']['videoCount']
+        views = items[0]['statistics']['viewCount']
+        print(title, description, url, subscribers, video, views)
+        return cls(channel_id, title, description, url, subscribers, video, views)
+    def to_json(self, filename):
+        data = {
+            'title': self.title,
+            'description': self.description,
+            'url': self.url,
+            'subscribers': self.subscribers,
+            'number of videos': self.video,
+            'total views': self.views,
+        }
+        with open(filename, 'w') as f:
+            json.dump(data, f)
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
         channel = youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
