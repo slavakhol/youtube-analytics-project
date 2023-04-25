a30c3b72d50fabe23076a4314937b5aac3aaae44
Slava Kholopov <slava.kholopov@gmail.com>
2023-04-25 10:22:39 +0200

    Добавил кастомный класс ошибки, который инициализируется, если запрошено несуществующее видео

--- src/video.py
+++ src/video.py
@@ -1,20 +1,39 @@
 from src.channel import Channel
+
 class Video():
     def __init__(self, id_video):
         youtube = Channel.get_service()
-        video_request = youtube.videos().list(
-            part='snippet,statistics',
-            id=id_video
-        )
-        video_response = video_request.execute()
-        self.id_video = id_video
-        self.title = video_response['items'][0]['snippet']['title']
-        self.url = f'https://www.youtube.com/video/{id_video}'
-        self.views = video_response['items'][0]['statistics']['viewCount']
-        self.likes = video_response['items'][0]['statistics']['likeCount']
+        try:
+            video_request = youtube.videos().list(
+                part='snippet,statistics',
+                id=id_video
+            )
+            video_response = video_request.execute()
+            if video_response['items'] == []:
+                raise NoVideoError
+
+        except NoVideoError as e:
+            print('Видео с таким  ID  не существует')
+            self.id_video = id_video
+            self.title = None
+            self.url = None
+            self.views = None
+            self.like_count = None
+
+        else:
+            self.id_video = id_video
+            self.title = video_response['items'][0]['snippet']['title']
+            self.url = f'https://www.youtube.com/video/{id_video}'
+            self.views = video_response['items'][0]['statistics']['viewCount']
+            self.like_count = video_response['items'][0]['statistics']['likeCount']
+
     def __str__(self):
         return self.title
 class PLVideo(Video):
     def __init__(self, id_video, id_playlist):
         super().__init__(id_video)
         self.id_playlist = id_playlist
+
+
+class NoVideoError(Exception):
+    pass
